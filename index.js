e4463a3709558dda09d0beed231a10a7d3341ff8
mrreider <rbrostoff@wisc.edu>
2021-10-27 15:06:09 -0500

    app.js working, no more react

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 import { AppRegistry } from 'react-native';
 import React from 'react';
 import App from './App';
-import { name as appName } from './app.json';
 
 
-const RNRedux = () => (
-    <App />
-)
-
-AppRegistry.registerComponent(appName);
+AppRegistry.registerComponent('RateMyRestroom', ()=> App);
