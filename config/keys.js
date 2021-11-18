87621de682c9bc7c2f14eefb24799115f3ff489d
amalapaka <60492889+amalapaka@users.noreply.github.com>
2021-11-17 20:04:19 -0600

    Added config folder

---
+++ config/keys.js
@@ -0,0 +1,13 @@
+export default {
+    firebaseConfig: {
+        apiKey: "AIzaSyAhOTQ1eMLl5SwVAixzfG_luxbJNiDby88",
+        authDomain: "ratemyrestroom-8e1a3.firebaseapp.com",
+        databaseURL: "https://ratemyrestroom-8e1a3-default-rtdb.firebaseio.com",
+        projectId: "ratemyrestroom-8e1a3",
+        storageBucket: "ratemyrestroom-8e1a3.appspot.com",
+        messagingSenderId: "11972517026",
+        appId: "1:11972517026:web:c338812449eeb763b60bfb",
+        measurementId: "G-480LTE0DNE"
+      },
+    googleMapsKey: "AIzaSyAm23sJcn7EuK-maawqFysYe0RrMsagawo"
+}
