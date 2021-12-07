9250ddec9e2250c3a68647b41e39497f228f965a
Soyoon Lee <slee985@wisc.edu>
2021-12-07 11:05:03 -0600

    welcome_page_styling_added

--- components/Welcome/Welcome.js
+++ components/Welcome/Welcome.js
@@ -1,58 +1,131 @@
-import {ImageBackground, StyleSheet, View, Text} from 'react-native';
-import React from 'react';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+ 
 export default function WelcomeScreen ({navigation}) {
-
-    const styles = StyleSheet.create({
-        button: {
-    
-        },
-        title: {
-            fontSize: 25,
-            fontWeight: 'bold',
-            marginTop: '50%',
-        },
-        titleContainer: {
-            flex: 1,
-            backgroundColor: '#fff',
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-        inlineText: {
-        },
-        buttonText: {
-            
-        },
-        background: {
-            flex: 1,
-            resizeMode: 'cover',
-            opacity: .2
-        }
-      });
-
   return (
-    <View style={styles.titleContainer}>
-        {/* <ImageBackground
-        style={styles.background}
-        source={require('../../assets/badger.png')}> */}
-            <Text style={styles.title}>Welcome to Rate My Restroom</Text>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Sign Up')} >
-                <Text style={styles.buttonText}>Sign Up</Text>
-            </TouchableOpacity>
-            <Text style={styles.inlineText}>Already have an account?</Text>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Sign In')}>
-                <Text style={styles.buttonText}>Sign In</Text>
-            </TouchableOpacity>
-      {/* </ImageBackground> */}
-    </View>
-    
-     
+    <View style={styles.container}>
+      <Image style={styles.logo_image} source={require("./assets/logo.png")} />
+
+      <Text style={styles.main_title_text}>👋 WELCOME 👋</Text>
+
+      <StatusBar style="auto" />
       
-    //    
-    //   
-    //   <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Sign In')}>
-    //     <Text style={styles.buttonText}>Sign In</Text>
-    //   </TouchableOpacity>
-  )
+      <Text style={styles.sign_up_text}>New User?</Text>
+
+      <TouchableOpacity style={styles.sign_up_button} onPress={() => navigation.navigate('Sign Up')}>
+          <Text style={styles.sign_up_button_text}>SIGN UP</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.sign_in_text}>Existing User?</Text>
+
+      <TouchableOpacity style={styles.sign_in_button} onPress={() => navigation.navigate('Sign In')}>
+          <Text style={styles.sign_in_button_text}>SIGN IN</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#DEDEDE',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  logo_image: {
+    top: "-9%",
+    
+    height: "32%",
+    width: "78%",
+    marginBottom: 40,
+  },
+
+  main_title_text: {
+    top: "-21.5%",
+
+    fontSize: 19,
+    fontWeight: "bold",
+    letterSpacing: 8,
+
+    textAlign: 'center',
+
+    color: "#07205B",
+  },
+
+  sign_up_text: {
+    top: "-14%",
+
+    fontSize: 21,
+    fontWeight: "bold",
+    letterSpacing: 4,
+
+    textAlign: 'center',
+
+    color: "#90251C",
+  },
+
+  sign_up_button: {
+    top: "-12.5%",
+
+    width: "45%",
+    height: "5%",
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+    backgroundColor: "#774C60",
+  },
+
+  sign_up_button_text: {
+    fontSize: 19,
+    fontWeight: "bold",
+    letterSpacing: 3,
+
+    textAlign: 'center',
+
+    color: "#00072D",
+  },
+
+  sign_in_text: {
+    top: "-6.5%",
+
+    fontSize: 21,
+    fontWeight: "bold",
+    letterSpacing: 4,
+
+    color: "#90251C",
+  },
+
+  sign_in_button: {
+    top: "-5%",
+
+    width: "45%",
+    height: "5%",
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+    backgroundColor: "#774C60",
+  },
+
+  sign_in_button_text: {
+    fontSize: 19,
+    fontWeight: "bold",
+    letterSpacing: 3,
+
+    textAlign: 'center',
+
+    color: "#00072D",
+  },
+});
